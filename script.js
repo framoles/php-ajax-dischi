6542778c69a4154de3da510e0e89535e3c9b772d
Francesco Moles <francesco.moles@outlook.it>
2022-05-05 22:11:06 +0200

    aggiunti commenti,qualche controllo e trasformato select statica in dinamica

--- script.js
+++ script.js
@@ -3,16 +3,36 @@ const app = new Vue({
     data() {
         return {
             array: Array,
-            genre: Array
+            genere: Array
         }
     },
     mounted() {
-        axios.get("http://localhost/php-ajax-dischi/database.php").then(risp => this.array = risp.data);
+        /* chiamata api su database.php + salvataggio dei generi disponibili su array */
+
+        axios.get("http://localhost/php-ajax-dischi/database.php").then((risp) => {
+            if (risp.status == 200) {
+                this.array = risp.data;
+                const support = [];
+                this.array.forEach((element) => {
+
+                    if (!support.includes(element.genre)) {
+                        support.push(element.genre);
+                    }
+
+                });
+                this.genere = support;
+            }
+        })
+            .catch(err => console.log(err));
 
     },
     methods: {
+        /* metodo per filtrare i generi tramite chiamata api con parametri  */
         displaySelected(event) {
-            axios.get("http://localhost/php-ajax-dischi/database.php", { params: { filtergenre: event.target.value } }).then(risp => this.array = risp.data);
+            axios.get("http://localhost/php-ajax-dischi/database.php", { params: { filtergenre: event.target.value } }).then(risp => {
+                if (risp.status == 200) { this.array = risp.data }
+            })
+                .catch(err => console.log(err));
         }
     }
 
